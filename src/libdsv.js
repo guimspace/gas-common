3a85265318cd76283bb242c11c9fb334bdfed345
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2019-02-14 13:10:40 -0200

    Build transform2DArrayToDsv

--- src/libdsv.js
+++ src/libdsv.js
@@ -24,3 +24,31 @@ function parseDsvTo2DArray_(source, delimiter) {
 
   return data;
 }
+
+
+function transform2DArrayToDsv_(data, delimiter) {
+  var output, i, j;
+
+
+  if(delimiter) {
+    output = data[0][0];
+
+    for(j = 1;  j < data[0].length;  j++) {
+      output += delimiter + data[0][j];
+    }
+
+    for(i = 1;  i < data.length;  i++) {
+      output += "\n";
+
+      output += data[i][0];
+      for(j = 1;  j < data[i].length;  j++) {
+        output += delimiter + data[i][j];
+      }
+    }
+
+  } else {
+    output = data.join("\n");
+  }
+
+  return output;
+}
