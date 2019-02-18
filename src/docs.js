42a517177ba0dd2a0e22f794547664f49efe584f
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2019-02-18 14:10:55 -0300

    Build bodyReplaceFirstText

--- src/docs.js
+++ src/docs.js
@@ -32,3 +32,46 @@ function bodyReplaceAllText_(body, list, sign) {
     body.replaceText(c1 + key + c2, list[key]);
   }
 }
+
+
+function bodyReplaceFirstText_(body, list, sign) {
+  var range, texto, index;
+  var key, c1, c2;
+
+  switch(sign) {
+    case "curly":
+      c1 = "{";
+      c2 = "}";
+      break;
+    case "round":
+      c1 = "(";
+      c2 = ")";
+      break;
+    case "square":
+      c1 = "[";
+      c2 = "]";
+      break;
+    case "at":
+      c1 = "@";
+      c2 = c1;
+      break;
+    case "percent":
+    default:
+      c1 = "%";
+      c2 = c1;
+      break;
+  }
+
+
+  for(key in list) {
+    if( !/\w+/.test(key) ) continue;
+
+    range = body.findText(c1 + key + c2);
+    if(!range) continue;
+
+    index = range.getStartOffset();
+    texto = range.getElement().asText();
+    texto.deleteText(index, range.getEndOffsetInclusive());
+    texto.insertText(index, list[key]);
+  }
+}
