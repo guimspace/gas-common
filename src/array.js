cd6464dfd78d571ef94f9f1cc973268d61a81493
Guilherme T Maeoka <gui.mspace@gmail.com>
2019-12-26 18:14:59 -0300

    Fix array tranpose for GAS ES5

--- src/array.js
+++ src/array.js
@@ -5,16 +5,22 @@
  */
 
 /**
- * https://stackoverflow.com/a/17428705
+ * Transpose a 2D array.
+ * @param  {array} src The array to be transposed.
+ * @return {array}     The tranposed array.
  */
-function transpose(array) {
-	array[0].map(
-		(col, i) => array.map(
-			row => row[i]
-		)
-	);
+function transpose(src) {
+	var dest = [ ];
+	var i, j;
 
-	return array;
+	for (i = 0; i < src[0].length; i++) {
+		dest.push([ ]);
+		for (j = 0; j < src.length; j++) {
+			dest[i].push(src[j][i]);
+		}
+	}
+
+	return dest;
 }
 
 
