458f650515ac37f15e78ce517b6853bc210848e2
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-04-04 07:37:50 -0300

    Replace listNameMonth with foo bar

--- src/head.js
+++ src/head.js
@@ -5,8 +5,5 @@ var APPS_SCRIPT = Object.freeze({
 		patch: 0
 	},
 
-	listNameMonth: [
-			[ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
-			[ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
-		]
+	foo: "bar"
 });
